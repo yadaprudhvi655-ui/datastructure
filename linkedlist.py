5b9d3abd1ea63ffd597f8b12dc2327f74b555b9e
prudhvi <yadaprudhvi655@gmail.com>
2026-06-22 15:32:50 +0530

    codechanged

--- linkedlist.py
+++ linkedlist.py
@@ -1,16 +1,33 @@
 class Node:
-    def _init_(self):
-        self.data=None
+    def __init__(self,data):
+        self.data=data
         self.next=None
+
 class Linkedlist:
-    def _inti_(self):
+    def __init__(self):
         self.head=None
-    def add_end(self.data):
+
+    def add_end(self,data):
         new=Node(data)
         if self.head is None:
-            self.head=new                
-        itr=self.head()
+            self.head=new
+            return
+
+        itr=self.head
         while itr.next:
             itr=itr.next
         itr.next=new
+
+    def display(self):
+        itr=self.head
+        while itr:
+            print(itr.data,end="-->")
+            itr=itr.next
+
+ll=Linkedlist()
+ll.add_end(50)
+ll.add_end(100)
+ll.add_end(150)
+ll.add_end(200)
+ll.display()            
             
